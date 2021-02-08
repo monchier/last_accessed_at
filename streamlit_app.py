f3088aa17ae4d00252b0ac01f2099a99f770aff4
Matteo Monchiero <matteo@streamlit.io>
2021-02-08 11:21:16 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 from dateutil.parser import parse
-from datetime import datetime
+import datetime
+
+one_week_ago = datetime.date.today() - datetime.timedelta(days=7)
 
 with open("last_accessed_at.txt", "r") as f:
     ts = [[parse(y) for y in x.strip().split("|")] for x in f.readlines()]
@@ -13,18 +15,20 @@ for i, row in enumerate(ts):
 df = pd.DataFrame(ts, columns=["created_at", "last_accessed_at"])
 df = df.reset_index()
 #st.write(df)
+line = alt.Chart(pd.DataFrame({'y': [str(one_week_ago)]})).mark_rule().encode(y='y:T')
 chart = alt.Chart(df).mark_circle().encode(
     x="created_at:T",
     y="last_accessed_at:T"
 )
 
-st.write(chart)
+st.altair_chart(chart + line, use_container_width=True)
 
 all_apps = df.count()
-df_count = df[df['last_accessed_at'] < "2021-1-29 12:00:00 PM"]
+#df_count = df[df['last_accessed_at'] < "2021-1-29 12:00:00 PM"]
+df_count = df[df['last_accessed_at'] < str(one_week_ago)]
 old_apps = df_count.count()
 
 
-st.write("Old means that they have not been accessed at least since the last release (2021-1-29)")
+st.write(f"Old means that they have not been accessed for a week ({one_week_ago})")
 st.write("Old apps:", old_apps)
 st.write("Fraction of old apps:", old_apps / all_apps)
